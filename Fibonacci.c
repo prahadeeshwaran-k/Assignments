cfe1ea7ace01805abd7b81a292562e97c930eb73
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-06-16 22:48:01 +0530

    fix

--- Fibonacci.c
+++ Fibonacci.c
@@ -4,9 +4,11 @@
 // Description: This program generates the Fibonacci series up to a given number of terms.
 #include<stdio.h>
 int main(){
+    
     int i=0, j=1,x,result;
     printf(" enter how many series:");
     scanf("%d",&x);
+
         while (x != 0)
         {   
             printf("%d ",i);
@@ -17,5 +19,5 @@ int main(){
 
             x--;
         }
-    
+        
 }
