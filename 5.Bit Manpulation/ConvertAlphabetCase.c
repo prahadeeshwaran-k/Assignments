ec9876def3a20fe4a13b46b2e648a47d18a37335
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-06-20 23:05:22 +0530

    Convert Alphabet Case using Bit Manipulation

---
+++ 5.Bit Manpulation/ConvertAlphabetCase.c
@@ -0,0 +1,22 @@
+//date:20/6/2025-sat
+//chapter:control statement
+//Title: Convert Alphabet Case using Bit Manipulation
+//Description: This program converts the case of a given alphabet using bit manipulation.
+#include<stdio.h>
+int main(){
+    char alph,mask,result;
+    printf("Enter the Alphabet: ");
+    scanf("%c",&alph);
+
+    if(alph >= 'A' && 'Z'<=alph || (alph >= 'a' && alph <= 'z') ){
+        printf("Before %c:",alph);
+        mask = (1<<5);
+        result = alph^mask;//else use alph^32 is same
+        printf("\nAfter %c:",result);
+    }
+    else
+    {
+        printf("Error: Not an alphabet character. Cannot convert case.\n");
+    }
+    
+}
