f5a00e7f20b4cb5b75d7e1b6f7916ecab1a54c79
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2025-06-26 13:59:50 +0530

    Update FloatBinarySintPtr.c

--- 6.Pointers/FloatBinarySintPtr.c
+++ 6.Pointers/FloatBinarySintPtr.c
@@ -1,4 +1,3 @@
-//date:25/6/2025-Wed
 //chapter:Pointers
 //Title: Float Binary using short int pointer
 // Description: This program generates the binary representation of a float value using a short int pointer.
@@ -8,14 +7,12 @@ int main(){
     printf("Enter a Float Value: ");
     scanf("%f",&f);
 
-    short int  * fptr = (short int *)&f;
-
+    short int * fptr = (short int *)&f;
+    fptr += 1;
     for(int j =1;j<=2;j++){
         for(int i = 15; i>=0; i--)
         {
         printf("%d",((*fptr)>>i)&1);
         }
-        fptr++;
+        fptr--;
     }
-    
-}
