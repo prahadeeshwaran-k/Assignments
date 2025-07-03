c1485ea2893e646caba330ade54f3b1d6a4de5de
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-07-03 19:04:39 +0530

    Number of occurrence

---
+++ 8.Strings/NumofOccurrence.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main(){
+    char str[100];
+    printf("enter the String: ");
+    scanf("%s",str);//Should be: scanf("%s", str); No & is needed with character arrays.
+    int n = 0 ;
+    while(str[n]){
+        n++;
+    }
+
+    int flag,count;
+    for(int i = 0;i<n-1;i++){
+        flag  = 0 ;
+        count = 1 ;
+        for(int j = 0; j<i;j++){
+            if(str[i] == str[j]){
+                flag = 1;
+                break;
+            }
+        }
+        if(flag){
+            continue;
+        }
+        for(int j = i+1; j < n; j++)
+        {
+            if(str[j]==str[i]){
+                count++;
+            }
+        }
+        if(count){
+            printf("\n%c = %d",str[i],count);
+        }
+        
+    }    
+}
