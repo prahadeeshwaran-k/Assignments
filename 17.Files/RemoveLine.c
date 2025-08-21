120ee3b452479226536c9f4d9dd04ef9321b15a7
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2025-08-21 12:50:37 +0530

    Update RemoveLine.c

--- 17.Files/RemoveLine.c
+++ 17.Files/RemoveLine.c
@@ -1,33 +1,56 @@
 //Write a Program to remove a Specific line from the given text file
 #include<stdio.h>
 #include<stdlib.h>   
+#define MAXLINE 2048
 
 int main(int argc, char* argv[]){
-    if(argc<3){
+    if(argc < 2){
         return 0;
     }
-    int count = 1;   // start line numbering from 1
-    int line = atoi(argv[2]);
-    int ch;          // must be int for EOF
+    
+    int count = 1;
+    int line;
+    char buffer[MAXLINE];
+    
+    int Tlines = 0;
 
     FILE *fp = fopen(argv[1],"r");
-    FILE *fdest = fopen("temp.txt","w");
-    
-    while((ch = fgetc(fp)) != EOF){
-        if(count == line){
-            // skip this line completely
-            while ((ch = fgetc(fp)) != EOF && ch != '\n');
-            count++;
-            continue;   // don't write skipped chars!
-        }
-        fputc(ch, fdest);
-        
-        if(ch == '\n'){
-            count++;
-        }
+    while((fgets(buffer, MAXLINE, fp)) != NULL){
+        Tlines++;
+        printf("%d %s",Tlines,buffer);
     }
+    if(!Tlines){
+        printf("file is empty\n");
+        return 0;
+    }
+    rewind(fp);
+    
+    FILE *fdest = fopen("temp.txt","w");
+    printf("enter the line to remove: ");
+    if (scanf("%d",&line), (line > Tlines || line < 1)){
+        printf("Invalid Line remove Operation\n");
+        return 0;
+    }
+    
+    while((fgets(buffer, MAXLINE, fp)) != NULL){
+        if(count != line){
+            fputs(buffer, fdest);
+        }
+        count++;
+    }
+
     fclose(fp);
     fclose(fdest);
     remove(argv[1]);
     rename("temp.txt", argv[1]);
+
+    fp = fopen(argv[1], "r");
+    Tlines = 0;
+    while((fgets(buffer, MAXLINE, fp)) != NULL){
+        Tlines++;
+        printf("%d %s",Tlines,buffer);
+    }
+    fclose(fp);
+
+    printf("removed the %d line of %s file\n", line, argv[1]);
 }
