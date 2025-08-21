16b833430b96eccf0ae0cb4f784a0defeabff094
Prahadeeshwaran <prahadeeshwaran.mk@gmail.com>
2025-08-21 13:50:18 +0530

    Create ReplaceLine.c

---
+++ 17.Files/ReplaceLine.c
@@ -0,0 +1,62 @@
+//Write a Program to replace a Specific line from the given text file
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define MAXLINE 2048
+
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		return 0;
+	}
+
+	int count = 1;   // start line numbering from 1
+	int line;
+	char buffer[MAXLINE];
+	char sentence[MAXLINE];
+	int Tlines = 0;
+
+	FILE *fp = fopen(argv[1],"r");
+	while((fgets(buffer, MAXLINE, fp)) != NULL) {
+		Tlines++;
+		printf("%d %s",Tlines,buffer);
+	}
+	if(!Tlines) {
+		printf("file is empty\n");
+		return 0;
+	}
+	rewind(fp);
+
+	FILE *fdest = fopen("temp.txt","w");
+	printf("enter the line to remove: ");
+	if (scanf("%d",&line), (line > Tlines || line < 1)) {
+		printf("Invalid Line remove Operation\n");
+		return 0;
+	}
+	printf("enter the String: ");
+	scanf(" %[^\n]s", sentence);
+
+	while((fgets(buffer, MAXLINE, fp)) != NULL) {
+		if(count != line) {
+			fputs(buffer, fdest);
+		} else {
+			sentence[strlen(sentence)] = '\n';
+			fputs(sentence, fdest);
+		}
+		count++;
+	}
+
+	fclose(fp);
+	fclose(fdest);
+	remove(argv[1]);
+	rename("temp.txt", argv[1]);
+
+	fp = fopen(argv[1], "r");
+	Tlines = 0;
+	while((fgets(buffer, MAXLINE, fp)) != NULL) {
+		Tlines++;
+		printf("%d %s",Tlines,buffer);
+	}
+	fclose(fp);
+
+	printf("removed the %d line of %s file\n", line, argv[1]);
+}
