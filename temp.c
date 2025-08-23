1b4cfcf247586094e721b890ae3d02a1e1b0b662
prahadeeshwaran-k <prahadeeshwaran.mk@gmail.com>
2025-08-23 21:50:36 +0530

    find the logic

--- temp.c
+++ temp.c
@@ -1,8 +1,45 @@
-//tempcode
-#include<stdio.h> 
-#include<string.h>
-    int main() {
-        printf("\033[1;31mThis text is bold red.\033[0m\n"); // Bold red, then reset
-        printf("\033[0;34mThis text is blue.\033[0m\n");     // Blue, then reset
-        return 0;
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+
+int main() {
+    char key[] = "Never";
+    char pare[] = "hi";
+    char buffer[256];
+    char buffer2[256] = {0}; 
+    FILE *fp = fopen("text.txt", "r");
+    FILE *fd = fopen("revs.txt", "w");
+
+    
+    if (fp == NULL) {
+        printf("File not found!\n");
+        return 1;
     }
+    system("cat text.txt");
+    printf("\n");
+    char* ptr = buffer; 
+    char* dest = buffer2;
+    char* temp;
+
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        while ((temp = strstr(ptr, key)) != NULL) {
+        
+        strncpy(dest, ptr, temp - ptr);
+        dest += (temp - ptr);
+
+        strcpy(dest, pare);
+        dest += strlen(pare);
+
+        ptr = temp + strlen(key);
+        }
+        strcpy(dest, ptr);
+        fputs(buffer2,fd);
+    }
+
+    fclose(fd);
+    fclose(fp);
+    system("cat revs.txt");
+
+    printf("\n");
+    return 0;
+}
